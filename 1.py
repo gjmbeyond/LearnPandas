a975773b8894ad132fab2a69c3dfe6bcbcabe1fc
gjmbeyond <365150309@outlook.com>
2024-02-29 20:40:20 +0800

    Sync with Collab

--- 1.py
+++ 1.py
@@ -1,1 +1,17 @@
-print("1")
+checked_out=["Andrea", "Manuel", "Khalid"]
+temp_list=[]
+
+with open("guests.txt", "rt") as guests:
+    for g in guests:
+        temp_list.append(g.strip())
+
+with open("guests.txt", 'w') as guests:
+    for name in temp_list:
+        if name not in checked_out:
+            guests.write(name + "\n")
+
+guests.close()
+
+with open("guests.txt") as guests:
+    for line in guests:
+        print(line)
